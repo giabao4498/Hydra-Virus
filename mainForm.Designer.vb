75a9c7fdafb2100c49413ea9efbdf1c618afcf57
Nguyễn Hoàng Gia Bảo <giabao441998@gmail.com>
2019-09-16 20:12:49 +0700

    Add files via upload

---
+++ mainForm.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(mainForm))
+        Me.oathLabel = New System.Windows.Forms.Label()
+        Me.author = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'oathLabel
+        '
+        Me.oathLabel.AutoSize = True
+        Me.oathLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.oathLabel.Location = New System.Drawing.Point(213, 28)
+        Me.oathLabel.Name = "oathLabel"
+        Me.oathLabel.Size = New System.Drawing.Size(0, 26)
+        Me.oathLabel.TabIndex = 0
+        Me.oathLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'author
+        '
+        Me.author.AutoSize = True
+        Me.author.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.author.Location = New System.Drawing.Point(408, 124)
+        Me.author.Name = "author"
+        Me.author.Size = New System.Drawing.Size(244, 20)
+        Me.author.TabIndex = 1
+        Me.author.Text = "Author: Nguyen Hoang Gia Bao"
+        Me.author.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(144, 144)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(732, 178)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.author)
+        Me.Controls.Add(Me.oathLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "mainForm"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hydra Virus"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents oathLabel As Label
+    Friend WithEvents author As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ mainForm.vb
@@ -0,0 +1,29 @@
+﻿Imports System.ComponentModel
+
+Public Module GlobalVariables
+    Public isFirstForm As Boolean = True
+End Module
+
+Public Class mainForm
+    Private Sub mainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        oathLabel.Text = "Cut off one head, two more shall take its place!" + Environment.NewLine + "Hail Hydra!"
+
+        If (isFirstForm = True) Then
+            isFirstForm = False
+        Else
+            Randomize()
+            Me.Location = New Point(Rnd() * (Screen.PrimaryScreen.Bounds.Width - Me.Width + 1),
+                                    Rnd() * (Screen.PrimaryScreen.Bounds.Height - Me.Height + 1))
+        End If
+
+        System.Media.SystemSounds.Hand.Play()
+    End Sub
+
+    Private Sub mainForm_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Me.Hide()
+        Dim newForm1 As New mainForm, newForm2 As New mainForm
+        newForm1.Show()
+        newForm2.Show()
+        e.Cancel = True
+    End Sub
+End Class
